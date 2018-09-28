b091c469775261bbebd956d385dc85be7dc7299f
FowleyT <FowleyT@users.noreply.github.com>
2018-09-28 09:04:26 +0000

    Delete Test_LCA.py
    Switching to Java

--- Test_LCA.py
+++
@@ -1,9 +0,0 @@
-import LowestCommonAncestor
-root = LowestCommonAncestor.Node(10)      #node0
-root.left = LowestCommonAncestor.Node(7)  #node1
-root.left.left = LowestCommonAncestor.Node(3)    #node2
-root.left.right = LowestCommonAncestor.Node(8)   #node3
-root.right = LowestCommonAncestor.Node(11)       #node4
-def test_LCA():
-LCAnode = LowestCommonAncestor.findLCA(root,root.left.left,root.left.right)
-assert(self.LCAnode.key == 7 )
