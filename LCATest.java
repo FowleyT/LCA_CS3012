229974b2247a57fc0dd2c6d7dc1f89d740b02632
FowleyT <FowleyT@users.noreply.github.com>
2018-10-02 09:16:28 +0000

    Update LCATest.java
    100% code coverage and working for all cases

--- LCATest.java
+++ LCATest.java
@@ -1,14 +1,17 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
- class LCATest {
- @Test 
+
+class LCATest {
+
+@Test 
 public void testConstructor() {
 	LCA tree = new LCA(); 
     tree.root = new Node(1); 
     tree.root.left = new Node(2); 
     tree.root.right = new Node(3);
 }
- @Test
+
+@Test
 public void testLCA() {
 	LCA tree = new LCA(); 
     tree.root = new Node(1); 
@@ -23,12 +26,29 @@ public void testLCA() {
     assertEquals("Checking with working tree" ,1, tree.findLCA(4,6));
     assertEquals("Checking with working tree" ,1, tree.findLCA(3,4));
     assertEquals("Checking with working tree" ,2, tree.findLCA(2,4));
- }
- @Test
+
+}
+
+@Test
 public void nullRoot() {
 	LCA tree = new LCA(); 
-    tree.root.left = new Node(2); 
-    tree.root.right = new Node(3);
-    assertEquals("Checking with null rooted tree" ,false, tree.findLCA(2,3));
+	tree.root = new Node(0);
+	tree.root = null;
+    assertEquals("Checking with null rooted tree" ,-1, tree.findLCA(0,0));
 }
- }
+
+@Test
+public void missingNodes() {
+	LCA tree = new LCA(); 
+	tree.root = new Node(0);
+	tree.root.left = new Node(2); 
+    tree.root.right = new Node(3);
+    tree.root.left.right = new Node(5); 
+    tree.root.right.left = new Node(6); 
+    assertEquals("Checking with n1 missing" ,-1, tree.findLCA(1,2));
+    assertEquals("Checking with n2 missing" ,-1, tree.findLCA(3,1));
+    assertEquals("Checking with n1 present" , 3, tree.findLCA(3,6));
+    assertEquals("Checking with n1 path >0" , 0, tree.findLCA(6,0));
+}
+
+}
